d8fe3fa272a15a660767e8ff81b9ac37c688ccbe
juan-el-capibara <juancampus23@gmail.com>
2024-04-18 06:39:46 -0500

    feat: :tada: Objeots
    Objetos

---
+++ script.js
@@ -0,0 +1,23 @@
+const persona = {
+    nombre: "Juan",
+    edad: 30,
+    genero: "masculino",
+    ocupacion: "ingeniero"
+  };
+  const coche = {
+    marca: "Toyota",
+    modelo: "Corolla",
+    año: 2022,
+    color: "rojo"
+  };
+  const libro = {
+    titulo: "El señor de los anillos",
+    autor: "J.R.R. Tolkien",
+    añoPublicacion: 1954,
+    genero: "Fantasía"
+  };
+  const numero = 42;
+  function suma(a, b) {
+    return a + b;
+  }
+      
